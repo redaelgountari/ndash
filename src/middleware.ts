1bd30ea18c351d8c8e726586bfae3770e982b32f
reda <redaelgountari7@gmail.com>
2024-08-26 17:29:52 +0200

    up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token");
+  // const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token");
 
-  if (token && request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
-  }
+  // if (token && request.nextUrl.pathname === "/") {
+  //   return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+  // }
 
-  if (!token && request.nextUrl.pathname.startsWith("/admin")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (!token && request.nextUrl.pathname.startsWith("/admin")) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   return NextResponse.next();
 }
@@ -17,3 +17,4 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/", "/admin/:path*"],
 };
+

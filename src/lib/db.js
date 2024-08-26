09521b943fd77fe0e24c9891485377b931fd3e68
reda <redaelgountari7@gmail.com>
2024-08-26 16:24:32 +0200

    fix conf next-auth

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,13 +3,10 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-// Create a new pool instance
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL, // Ensure this environment variable is set in Vercel
+  connectionString: process.env.POSTGRES_URL, 
 });
 
-// Function to query the database
 export const query = (text, params) => pool.query(text, params);
 
-// Optional: Export the pool for more advanced use
 export default pool;

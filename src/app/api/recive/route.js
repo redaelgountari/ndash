e2d76ac44b9bb0e3ad97ca25075799ec2b32fea8
reda <redaelgountari7@gmail.com>
2024-08-27 11:44:44 +0200

    remove Cache

--- src/app/api/recive/route.js
+++ src/app/api/recive/route.js
@@ -7,13 +7,21 @@ export async function GET(req) {
   try {
     const getQuery = await client.sql`SELECT * FROM reports`;
     const getTitles = await client.sql`SELECT DISTINCT(titre) FROM reports`;
-    
+
     return NextResponse.json({
       data: getQuery.rows,
       titles: getTitles.rows,
+    }, {
+      headers: {
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+      }
     });
   } catch (error) {
     console.error('Error fetching data:', error);
     return new NextResponse('Error fetching data', { status: 500 });
-  } 
+  } finally {
+    client.release(); 
+  }
 }
